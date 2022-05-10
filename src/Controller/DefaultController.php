683b6650fbc136b9882a5433f56abdbacceefa99
MARTIENNE Nicolas <nicolas.martienne@sn-annuaire.naz>
2022-05-10 16:34:28 +0200

    Création du tableau des favoris avec affichage des données de Favoris

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Favoris;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'home', methods: ['GET', 'POST'])]
+    public function index(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $favoris = $doctrine->getRepository(Favoris::class)->findAll();
+
+
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'favoris' => $favoris,
+        ]);
+    }
+}
