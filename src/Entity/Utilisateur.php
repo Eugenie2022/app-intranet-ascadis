a3d1e3f54b08e572c432f506ddd7c9e4c8311085
MARTIENNE Nicolas <nicolas.martienne@sn-annuaire.naz>
2022-05-11 13:41:20 +0200

    Ajout du CRUD entité Favoris et mise en forme favoris/index

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -148,4 +148,9 @@ class Utilisateur implements UserInterface , PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->username;
+    }
 }
